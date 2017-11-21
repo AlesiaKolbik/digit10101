426e111beab91087a94831c1d8725c845913dc35
Alesia Kolbik <moyaalesia@mail.ru>
2017-11-21 23:05:48 +0300

    Initial commit

---
+++ src/com/testpackage/Main.java
@@ -0,0 +1,18 @@
+package com.testpackage;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner inputDigit = new Scanner(System.in);
+        System.out.println("Введите натуральное число:");
+        int digit = inputDigit.nextInt();
+        for (int i = 1; i < digit; i++) {
+            for (int j = 1; j < digit; j++) {
+                int productOfNumbers = i * j;
+
+            }
+        }
+    }
+}
