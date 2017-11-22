14b29e7c9c9783b3b889b937f44f8152aa76f68c
alesya.kolbik <moyaalesia@mail.ru>
2017-11-22 11:24:05 +0300

    add functions

--- src/com/testpackage/Main.java
+++ src/com/testpackage/Main.java
@@ -8,11 +8,39 @@ public class Main {
         Scanner inputDigit = new Scanner(System.in);
         System.out.println("Введите натуральное число:");
         int digit = inputDigit.nextInt();
-        for (int i = 1; i < digit; i++) {
-            for (int j = 1; j < digit; j++) {
-                int productOfNumbers = i * j;
-
+        for (int i = 2; i < digit; i++) {
+            for (int j = 2; j < digit; j++) {
+                int multiTwoNumbers=multiplication(i,j);
+                String multiTwoNambers=String.valueOf(multiTwoNumbers);
+                int maxDigit=maxDigit(i,j);
+                String number=addNullToString(maxDigit);
+                if(multiTwoNambers.equals(number)){
+                    System.out.println(i+"*"+j+"="+multiTwoNumbers);
+                }
             }
         }
     }
+    public static int maxDigit(int number1, int number2) {
+        if (number1 > number2) {
+            return number1;
+        } else {
+            return number2;
+        }
+    }
+    public static int multiplication(int number1, int number2) {
+        int result=number1*number2;
+        return  result;
+    }
+    public static String addNullToString (int number){
+        String createStringToNumber=String.valueOf(number);
+        int length=createStringToNumber.length();
+        String rezult=new String();
+        for(int i=0;i<length-1;i++){
+            rezult+=createStringToNumber.charAt(i);
+            rezult+='0';
+        }
+        rezult+=createStringToNumber.charAt(length-1);
+        return rezult;
+    }
+
 }
